edff53e3a48c153ff6789cc702c947164e83aff0
Naitik2408 <120440558+Naitik2408@users.noreply.github.com>
2023-02-27 22:30:31 +0530

    webtask

---
+++ index.js
@@ -0,0 +1,112 @@
+var a;
+function show_hide()
+{
+    if(a==1)
+    {
+        document.getElementById("to").style.display="none";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("to").style.display="inline";
+        return a=1;
+    }
+}
+
+
+var b;
+function show_hide2()
+{
+    if(b==1)
+    {
+        document.getElementById("to2").style.display="none";
+        return b=0;
+    }
+
+    else
+    {
+        document.getElementById("to2").style.display="inline";
+        return b=1;
+    }
+}
+
+var c;
+function show_hide3()
+{
+    if(c==1)
+    {
+        document.getElementById("to3").style.display="inline";
+        return c=0;
+    }
+
+    else
+    {
+        document.getElementById("to3").style.display="none";
+        return c=1;
+    }
+}
+
+var d;
+function show_hide4()
+{
+    if(d==1)
+    {
+        document.getElementById("to4").style.display="inline";
+        return d=0;
+    }
+
+    else
+    {
+        document.getElementById("to4").style.display="none";
+        return d=1;
+    }
+}
+
+var e;
+function show_hide5()
+{
+    if(e==1)
+    {
+        document.getElementById("to5").style.display="inline";
+        return e=0;
+    }
+
+    else
+    {
+        document.getElementById("to5").style.display="none";
+        return e=1;
+    }
+}
+
+var f;
+function show_hide6()
+{
+    if(f==1)
+    {
+        document.getElementById("to6").style.display="inline";
+        return f=0;
+    }
+
+    else
+    {
+        document.getElementById("to6").style.display="none";
+        return f=1;
+    }
+}
+
+var g;
+function show_hide7()
+{
+    if(g==1)
+    {
+        document.getElementById("to7").style.display="none";
+        return g=0;
+    }
+
+    else
+    {
+        document.getElementById("to7").style.display="inline";
+        return g=1;
+    }
+}
